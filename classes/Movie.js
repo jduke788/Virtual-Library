eeba81a697fc987e27d1a531ede6840b06921b92
jduke788 <jduke788@gmail.com>
2024-10-01 12:49:44 +0100

    static method need finished

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,18 @@
 // import the Media class:
-
+const Media = require("./Media")
 // create your Movie class:
+class Movie extends Media {
+    constructor(title, genre, year, director, duration, rating) {
+        super(title, genre, year)
+        this.director = director
+        this.duration = duration
+        this.rating = rating
+    }
+    summary() {
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Duration: ${this.duration}, Rating: ${this.rating}`
+    }
 
+    static longestMovie 
+}
 // don't change below
 module.exports = Movie;
